2d914636f121032d5cd78a640b1d915851c812c6
VictoriaDudu <dyudyunova@inbox.ru>
2024-03-28 10:05:52 +0700

    счет чисел в строке

---
+++ vowel.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+const int N = 12;
+char vowel[N] = { 'a', 'e', 'i', 'o', 'u', 'y' };
+string str;
+cout << "Enter the string" << endl;
+getline(cin, str);
+int kolichestvo = 0;
+for (int i = 0; str[i]; i++)
+{
+for (int j = 0; j < N; j++)
+{
+if (str[i] == vowel[j])
+kolichestvo++;
+}
+}
+if (kolichestvo)
+cout << "Nomber of vowels: " << kolichestvo << endl;
+else
+cout << "No vowels" << endl;
+return 0;
+}
